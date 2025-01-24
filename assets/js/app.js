dc40c748ee2e277288779acc5bc15d0382b8abdd
Roshan Bende <rosh18ben@gmail.com>
2025-01-24 11:33:55 +0530

    first for project

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+
+
+const searchSvg = document.getElementById('searchSvg');
+const searchInput = document.getElementById('searchInput');
+
+searchSvg.addEventListener('click', function (event) {
+    searchInput.style.width = '90%';
+    searchInput.style.padding = '0.5rem 1rem';
+    searchInput.focus(); 
+    event.stopPropagation();
+  });
+  searchInput.addEventListener('click', function (event) {
+    searchInput.style.width = '90%';
+    searchInput.style.padding = '0.5rem 1rem';
+    searchInput.focus(); 
+    event.stopPropagation();
+  });
+
+
+  document.addEventListener('click', function () {
+    searchInput.style.width = '0';
+    searchInput.style.padding = '0';
+
+  });
+
+
+  // const welcomeText = document.getElementById("welcomeText");
+  // const welcomeText = document.getElementById("welcomeText");
+
+  // const observer = new IntersectionObserver(
+  //   ([entry]) => {
+  //     if (!entry.isIntersecting) {
+  //       welcomeText.style.fontSize = "1rem"; // Change to your desired color
+  //     } else {
+  //       welcomeText.style.fontSize = "2rem"; // Reset to default color
+  //     }
+  //   },
+  //   {
+  //     root: null,
+  //     threshold: 0, // Triggers when the element is fully or partially out of view
+  //   }
+  // );
+
+  // observer.observe(welcomeText);
